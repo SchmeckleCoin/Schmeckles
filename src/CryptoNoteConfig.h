dc6de3dca827df7eac0630e975f8328f8fea7b99
Nodes <33381653+TheNodeCoin@users.noreply.github.com>
2017-11-07 14:08:05 -0700

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -12,7 +12,7 @@ namespace parameters {
 
 const uint64_t CRYPTONOTE_MAX_BLOCK_NUMBER                   = 500000000;
 const size_t   CRYPTONOTE_MAX_BLOCK_BLOB_SIZE                = 500000000;
-const size_t   CRYPTONOTE_MAX_TX_SIZE                        = 1000000000;
+const size_t   CRYPTONOTE_MAX_TX_SIZE                        = 10000000000000000;
 //TODO Currency-specific address prefix
 const uint64_t CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX       = 0x2;
 //TODO Choose maturity period for your currency
