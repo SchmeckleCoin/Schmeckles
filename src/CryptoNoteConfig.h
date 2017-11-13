9e1fe4a1b25cb0544a9a2319fc9adcad7e40972b
SchmeckleCoin <33556127+SchmeckleCoin@users.noreply.github.com>
2017-11-13 03:13:35 -0700

    Update CryptoNoteConfig.h
    Changed address prefix

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -14,7 +14,7 @@ const uint64_t CRYPTONOTE_MAX_BLOCK_NUMBER                   = 50000000000;
 const size_t   CRYPTONOTE_MAX_BLOCK_BLOB_SIZE                = 50000000000;
 const size_t   CRYPTONOTE_MAX_TX_SIZE                        = 1000000000000;
 //TODO Currency-specific address prefix
-const uint64_t CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX       = 0x9b;
+const uint64_t CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX       = 0x1b0f29c9519;
 //TODO Choose maturity period for your currency
 const size_t   CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW          = 60;
 const uint64_t CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT            = 60 * 60 * 2;
